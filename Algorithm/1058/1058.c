1d596209ff3fa25069a868e2e959ca74ba6106bb
qualificationalitated <rlagusdn9548@gmail.com>
2021-11-14 21:21:31 +0900

    알고리즘 문제풀이
    1058_친구
    A의 친구이거나, 한다리 건너 친구를 깐부라고 할 때, 깐부의 수가 가장 많은 사람의 깐부 수를 찾는 문제
    우선 배열을 큰 값으로 초기화 해 준뒤, 모든 값을 탐색한다.
    i,j 위치에 도달 할 때, 그냥 I,J를 가는거보다 k를 거치고 가면 빠른 경우를 찾아 값을 갱신해준다.
    본 문제에서는 두 다리 걸쳐서 친구를 찾고싶으므로, 가야 할 거리가 2 이하인 경우만 찾으면 된다.

---
+++ Algorithm/1058/1058.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<string.h>
+int map[52][52],n,max=0;
+int main()
+{
+    char s[52];
+    scanf("%d",&n);
+    
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            map[i][j]=3;
+    for (int i = 0; i < n; i++){
+        scanf("%s",s);
+        for (int j = 0; j < n; j++)
+            if(s[j]=='Y')
+                map[i][j]=1;
+    }
+    for (int k = 0; k < n; k++)
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                if(map[i][j]>map[i][k]+map[k][j])
+                    map[i][j]=map[i][k]+map[k][j];
+    for (int i = 0; i < n; i++){
+        int cnt=0;
+        for (int j = 0; j < n; j++)
+            if(i!=j&&(map[i][j]==1||map[i][j]==2))
+                cnt++;
+        if(max<cnt)
+            max=cnt;
+    }
+    printf("%d",max);
+    return 0;
+}
+/**
+ * @brief 플로이드 마샬은 처음 써보는 것에 대하여..
+ * 플로이드 마샬 알고리즘을 활용해 문제를 풀었다.
+ * n^3 만큼의 시간복잡도를 가진다.
+ * 우선 배열을 큰 값으로 초기화 해 준뒤, 모든 값을 탐색한다.
+ * i,j 위치에 도달 할 때, 그냥 I,J를 가는거보다 k를 거치고 가면 빠른 경우를 찾아 값을 갱신해준다.
+ * 본 문제에서는 두 다리 걸쳐서 친구를 찾고싶으므로, 가야 할 거리가 2 이하인 경우만 찾으면 된다.
+ */
