6f624f79774d7047110a9973e024a45884f3739e
qualificationalitated <rlagusdn9548@gmail.com>
2022-01-14 18:18:55 +0900

    10844(쉬운 계단 수)풀이 해설 추가 및 코드 개선

--- Algorithm/10844/10844.c
+++ Algorithm/10844/10844.c
@@ -1,20 +1,18 @@
 #include<stdio.h>
-int main() {
-
-    int n,i,j;
-    int possible[101][11] = { 0, };
-    scanf("%d", &n);
-    for(i = 0; i <= 9; i++) 
-        possible[1][i] = 1;
-    for(i = 2; i <= n; i++) {
-        possible[i][0]=possible[i-1][1];
-        possible[i][10]=0;
+#define MAX 1000000000
+int main(){
+    int n,i,j,sum=0,dp[101][11]={0,};
+    scanf("%d",&n);
+    for(i=0;i<=9;i++) 
+        dp[1][i] = 1;
+    for(i=2;i<=n;i++) {
+        dp[i][0]=dp[i-1][1];
+        dp[i][10]=0;
         for (j=0;j<=9;j++)
-            possible[i][j]=(possible[i-1][j-1]+possible[i-1][j+1])%1000000000;
+            dp[i][j]=(dp[i-1][j-1]+dp[i-1][j+1])%MAX;
     }
-    int sum=0;
-    for (i = 1; i <= 9; i++)
-        sum = (sum + possible[n][i]) % 1000000000;
-    printf("%d\n", sum);
+    for (i=1;i<=9;i++)
+        sum=(sum+dp[n][i])%MAX;
+    printf("%d\n",sum);
     return 0;
 }
