33b3d6e079fa017959130d0c80aa043c51eff34b
qualificationalitated <rlagusdn9548@gmail.com>
2022-01-17 17:19:31 +0900

    11048(이동하기) 풀이 해설 추가 및 불필요 코드 제거

--- Algorithm/11048/11048.c
+++ Algorithm/11048/11048.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 int map[1001][1001],find[1001][1001],mx[3]={1,0,1},my[3]={0,1,1};
 int main(void){
- 
     int n, m;
     scanf("%d %d",&n,&m);
     for (int i = 1; i <= n; i++)
-        for (int j = 1; j <= m; j++)
-            scanf("%d", &map[i][j]);
-    for (int i = 1; i <= n; i++)
         for (int j = 1; j <= m; j++){
+            scanf("%d", &map[i][j]);
             int ans = 0;
             for (int k = 0; k < 3; k++)
             {
