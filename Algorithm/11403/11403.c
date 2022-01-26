30338697eb8f7215d19fd1bd879476940ea9bda1
qualificationalitated <rlagusdn9548@gmail.com>
2022-01-26 18:27:55 +0900

    11403(경로 찾기) 풀이 해설 추가 및 코드정리

--- Algorithm/11403/11403.c
+++ Algorithm/11403/11403.c
@@ -1,34 +1,24 @@
 #include<stdio.h>
-int map[102][102],n;
-int possible[102][102];
-int find(int root,int now)
-{
+#define LIM 102
+int n,map[LIM][LIM],goAble[LIM][LIM];
+int find(int x,int now){
 	for (int i = 0; i < n; i++)
-	{
-		if(map[now][i]==1&&possible[root][i]==0)
-		{
-			possible[root][i]=1;
-			find(root,i);
+		if(map[now][i]&&(!goAble[x][i])){
+			goAble[x][i]=1;
+			find(x,i);
 		}
-	}
 	return 0;
 }
-int main()
-{
+int main(){
 	scanf("%d",&n);
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < n; j++)
 			scanf("%d",&map[i][j]);
 	for (int i = 0; i < n; i++)
-	{
 		find(i,i);
-	}
-	for (int i = 0; i < n; i++)
-	{
+	for (int i = 0; i < n; i++){
 		for (int j = 0; j < n; j++)
-		{
-			printf("%d ",possible[i][j]);
-		}
+			printf("%d ",goAble[i][j]);
 		printf("\n");
 	}
 	return 0;
