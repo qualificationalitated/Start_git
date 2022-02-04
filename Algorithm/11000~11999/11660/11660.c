3983662e87cfa2f2b5fa7f9358921ffa911ec6de
qualificationalitated <rlagusdn9548@gmail.com>
2022-02-04 18:13:53 +0900

    11660(구간 합 구하기 5)풀이 해설 추가 및 코드수정
    코드 변경 내용
    
    #define 설정을 통한 배열 크기 관리
    기존의 열 값을 저장하는 배열을 단일 변수로 변경
    기타 코드 간소화

--- Algorithm/11000~11999/11660/11660.c
+++ Algorithm/11000~11999/11660/11660.c
@@ -1,23 +1,23 @@
 #include<stdio.h>
-int map[1025][1025],sum[1025];
+#define LIM 1025
+int numArr[LIM][LIM]={0,};
 int main()
 {
     int n,m;
+    int ax,ay,bx,by;
     scanf("%d %d",&n,&m);
-    for (int i = 1; i <= n; i++)
-    {
+    for (int i = 1; i <= n; i++){
+        int columnSum=0,x;
         for (int j = 1; j <= n; j++)
         {
-            scanf("%d",&sum[j]);
-            sum[j]+=sum[j-1];
-            map[i][j]=sum[j]+map[i-1][j];
+            scanf("%d",&x);
+            columnSum+=x;
+            numArr[i][j]=columnSum+numArr[i-1][j];
         }
     }
-    for (int i = 0; i < m; i++)
-    {
-        int ax,ay,bx,by;
-        scanf("%d%d%d%d",&ax,&ay,&bx,&by);
-        printf("%d\n",map[bx][by]+map[ax-1][ay-1]-map[bx][ay-1]-map[ax-1][by]);
+    while (m--){
+        scanf("%d %d %d %d",&ax,&ay,&bx,&by);
+        printf("%d\n",numArr[bx][by]+numArr[ax-1][ay-1]-numArr[bx][ay-1]-numArr[ax-1][by]);
     }
     return 0;
 }
