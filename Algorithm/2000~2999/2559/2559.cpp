1c9b18fd72478cb173bf7f083b18b8e13e41f5dd
qualificationalitated <rlagusdn9548@gmail.com>
2023-01-13 20:03:17 +0900

    알고리즘 문제풀이
    2559_수열 / S3

---
+++ Algorithm/2000~2999/2559/2559.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+#define LIM 100000000
+int main(){
+    int n,k,max=LIM*-1;
+    int input,arrSum[100002]={0,};
+    scanf("%d %d",&n,&k);
+    for(int i=1;i<=n;i++){
+        scanf("%d",&input);
+        arrSum[i]=arrSum[i-1]+input;
+        if(i>=k && max<arrSum[i]-arrSum[i-k]){
+            max=arrSum[i]-arrSum[i-k];
+        }
+    }
+    printf("%d",max);
+    return 0;
+}
