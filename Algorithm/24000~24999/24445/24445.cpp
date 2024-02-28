c42a8b07284b943eabca0d9308d56e95490321e2
qualificationalitated <hk23g@fsu.edu>
2024-02-29 01:55:58 +0900

    [add] 알고리즘 문제풀이
    24445_알고리즘 수업 - 너비 우선 탐색 2 / S2

---
+++ Algorithm/24000~24999/24445/24445.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+#define MAX 100002
+using namespace std;
+vector<int> edge[MAX];
+bool visited[MAX];
+int answer[MAX];
+int bfs(int start){
+	queue<int> v_list;
+	int order = 0;
+	visited[start] = true;
+	v_list.push(start);
+	while(v_list.empty() == false){
+		int now = v_list.front(); v_list.pop();
+		answer[now] = ++order;
+		for(int i=0;i<edge[now].size();i++)
+			if(visited[edge[now][i]] == false){
+				visited[edge[now][i]] = true;
+				v_list.push(edge[now][i]);
+			}
+	}
+	return 0;
+}
+int main(){
+	int n,m,r,u,v;
+	cin>>n>>m>>r;
+	while(m--){
+		cin>>u>>v;
+		edge[u].push_back(v);
+		edge[v].push_back(u);
+	}
+	for(int i=1;i<=n;i++)
+		sort(edge[i].begin(),edge[i].end(),greater<int>());
+	bfs(r);
+	for(int i=1;i<=n;i++)
+		cout<<answer[i]<<"\n";
+	return 0;
+}
